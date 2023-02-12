2332a8f0fec378200a964e514390b6c2148dd815
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-12 12:34:43 +0000

    java変数定義

---
+++ java3.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String[] args) {
+    int number ;
+    number = 31 ;
+    System.out.println(number);
+
+    String name ;
+    name = "Kashima" ;
+    System.out.println(name);
+  }
+}
