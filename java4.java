f294458321732a45d914b12f26310183183b7994
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-13 11:07:20 +0000

    java変数定義

---
+++ java4.java
@@ -0,0 +1,24 @@
+class Main{
+  public static void main(String[] args) {
+    int number = 50;
+    System.out.println(number);
+
+    String name = "Kashima";
+    System.out.println(name);
+
+    number = 31;
+    System.out.println(number);
+
+    name = "Yuuki";
+    System.out.println(name);
+
+    number += 9;
+    System.out.println(number);
+
+    number -=10;
+    System.out.println(number);
+
+    number ++;
+    System.out.println(number);
+  }
+}
