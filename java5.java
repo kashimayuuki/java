90a6b60840719284e33fbb06e6fe7b60fb7556fd
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-14 12:51:53 +0000

    java変数定義

---
+++ java5.java
@@ -0,0 +1,31 @@
+class Main{
+  public static void main(String[] args) {
+    int length = 6;
+    int height = 8;
+
+    // 変数rectangleAreaに、四角形の面積を代入してください
+    int rectangleArea = length * height;
+
+    // 変数rectangleAreaを出力してください
+    System.out.println(rectangleArea);
+
+    // 変数triangleAreaに、三角形の面積を代入してください
+    int triangleArea = length * height / 2;
+
+    // 変数triangleAreaを出力してください
+    System.out.println(triangleArea);
+
+    // double型の変数number1を定義し、8.5を代入してください。
+    double number1 = 8.5;
+
+    // double型の変数number2を定義し、3.4を代入してください。
+    double number2 = 3.4;
+
+    // number1にnumber2を足した値を出力してください
+    System.out.println(number1 + number2);
+
+    // number1からnumber2を引いた値を出力してください
+    System.out.println(number1 - number2);
+
+  }
+}
