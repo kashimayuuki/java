66c0588a587884e08a080593cd694300159a2a60
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-16 02:26:48 +0000

    java真偽値

---
+++ java2_1.java
@@ -0,0 +1,40 @@
+class Main {
+  public static void main(String[] args) {
+    // 「true」を用いて、「真」を表す真偽値を出力してください
+    System.out.println(true);
+
+    // 「false」を用いて、「偽」を表す真偽値を出力してください
+    System.out.println(false);
+
+    // 「==」を用いて、値を比較した結果を出力してください
+    System.out.println(12/4 == 3);
+
+    // 「!=」を用いて、値を比較した結果を出力してください
+    System.out.println(12/4 != 3);
+
+    // 変数を定義し、値を比較した結果を代入してください
+    boolean bool = 3*9 ==27;
+
+    // 変数boolの値を出力してください
+    System.out.println(bool);
+
+    System.out.println(5*2 < 9);
+
+    System.out.println( 5*3 <= 15);
+
+        // trueと出力されるようにしてください
+    System.out.println(true || false);
+
+    // falseと出力されるようにしてください
+    System.out.println(false && true);
+
+    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+    System.out.println(8<5 && 3>=2 );
+
+    // 「8 < 5」または「3 >= 2」の結果を出力してください
+    System.out.println(8<5 || 3>=2);
+
+    // 「8 < 5」でない、の結果を出力してください
+    System.out.println(!(8<5));
+  }
+}
