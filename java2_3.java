e068a9678332c2467715aa99e12534d209cf064a
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-18 13:38:45 +0000

    java真偽値

---
+++ java2_3.java
@@ -0,0 +1,13 @@
+class Main{
+  public static void main(String[]args){
+    int number = 15;
+
+    if(number < 10){
+      System.out.println("10より小さい");
+    }else if(number < 20){
+      System.out.println("10以上,20以下");
+    }else{
+      System.out.println("20以上");
+    }
+  }
+}
