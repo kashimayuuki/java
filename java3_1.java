4b791a49093ef41c94412a7f110175267c3374a7
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-02-27 13:02:03 +0000

    javaメソッド

--- java3_1.java
+++ java3_1.java
@@ -2,10 +2,10 @@ class Main{
   public static void main(String[]args){
     hello();
       // 引数に「Kate Jones」を渡してください
-    printData("Kate Jones",15);
+    printData("Kate Jones",15,180);
 
     // 引数に「John Christopher Smith」を渡してください
-    printData("John Christopher Smith", 60 );
+    printData("John Christopher Smith", 60 ,170 );
 
   }
   public static void hello(){
@@ -13,11 +13,13 @@ class Main{
   }
 
   // 引数を受け取るようにしてください
-  public static void printData(String name, int age) {
+  public static void printData(String name, int age ,int tall) {
     // 「私の名前は◯◯です」と出力されるように書き換えてください
     System.out.println("私の名前は"+ name +"です");
 
     System.out.println("年齢は"+age+"歳です");
+
+    System.out.println("身長は"+ tall + "cmです");
 
   }
 
