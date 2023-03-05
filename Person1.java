9f1957438ff27aed58d4066e55ea6f4a2b7887bd
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-03-05 12:41:12 +0000

    javaクラス

---
+++ Person1.java
@@ -0,0 +1,37 @@
+class Person1{
+  public static void printData(String name, int age, double height, double weight) {
+  System.out.println("私の名前は" + name + "です");
+  System.out.println("年齢は" + age + "歳です");
+  System.out.println("身長は" + height + "歳です");
+  System.out.println("体重は" + weight + "歳です");
+
+  double bmi = bmi(height,weight);
+
+  System.out.println("BMIは" + bmi + "です");
+  
+  if(isHealthy(bmi)){
+    System.out.println("健康です");
+  }else{
+    System.out.println("不健康です");
+  }
+
+  }
+
+  public static String fullName(String firstName, String lastName) {
+    return firstName + " " + lastName;
+  }
+
+  // fullNameメソッドを定義してください
+  public static String fullName(String firstName, String middleName, String lastName){
+    return firstName + " " + middleName + " " + lastName;
+  }
+
+  public static double bmi(double height, double weight){
+    return weight/height/height;
+  }
+  
+  public static boolean isHealthy(double bmi){
+    return bmi >= 18.5 && bmi < 25 ;
+  }
+
+}
---
+++ java3_5.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Person1.printData(Person1.fullName("kasima", "yuuki"), 31, 1.79, 84);
+
+    // fullNameメソッドを用いて、printDataの引数を書き換えてください
+    Person1.printData(Person1.fullName("John", "Christopher", "Smith"), 45,1.91, 90);
+  }
+}
