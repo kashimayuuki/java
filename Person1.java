cab3757b150f46087e7ddabbc9307e3dbc046d56
EC2 Default User <108597406+kashimayuuki@users.noreply.github.com>
2023-03-07 05:46:38 +0000

    javaメソッド

--- Person1.java
+++ Person1.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 class Person1{
   public static void printData(String name, int age, double height, double weight) {
   System.out.println("私の名前は" + name + "です");
@@ -7,8 +9,8 @@ class Person1{
 
   double bmi = bmi(height,weight);
 
-  System.out.println("BMIは" + bmi + "です");
-  
+  System.out.println("BMIは" + Math.round(bmi) + "です");
+
   if(isHealthy(bmi)){
     System.out.println("健康です");
   }else{
@@ -29,7 +31,7 @@ class Person1{
   public static double bmi(double height, double weight){
     return weight/height/height;
   }
-  
+
   public static boolean isHealthy(double bmi){
     return bmi >= 18.5 && bmi < 25 ;
   }
